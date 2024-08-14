22d9ca19e5820589bbc13b72801842b76e4b6362
Pedro <ddrppx@gmail.com>
2024-08-13 21:09:21 -0300

    functions.c .h, refactor, modulated, and comments/decorators

--- include/functions.h
+++ include/functions.h
@@ -5,42 +5,192 @@
 
 #include <time.h> // struct tm
 
-// Para o programa com o código de erro
+/**
+ * Para execução do programa com o código de erro
+ *
+ * @param err_code  Código do erro.
+ */
 void error_exit(int err_code);
 
-// Mostra a mensagem de erro com especificação do campo
+/**
+ * Mostra a mensagem de erro com especificação do campo
+ *
+ * @param msg Mensagem a ser impressa na tela.
+ * @param campo Campo onde ocorreu o erro, para caso falhe a validação.
+ *
+ * @returns 1 para válido, 0 para invalidado.
+ */
 void error_message_field(const char *msg, char *campo);
 
 // Mostra a mensagem de erro com especificação do id
+/**
+ * Mostra mensagem de erro com especificação do id.
+ *
+ * @param msg Mensagem a ser impressa na tela.
+ * @param id Número identificador da estrutura onde ocorreu o erro, para caso falhe a validação.
+ *
+ * @returns 1 para válido, 0 para invalidado.
+ */
 void error_message_id(const char *msg, int id);
 
-// Verifica se o inteiro id é válido, maior que zero
+/**
+ * Verifica se o número é maior ou igual a zero.
+ *
+ * @param num Variável time a ser testada.
+ * @param campo Campo onde ocorreu o erro, para caso falhe a validação.
+ *
+ * @returns 1 para válido, 0 para invalidado.
+ */
 int validate_id(int num, char *campo);
 
-// Verifica se o Dia, Mês e Ano é igual a zero  
+/**
+ * Verifica se o Dia, Mês e Ano são igual a zero.
+ *
+ * @param time Variável time a ser testada.
+ * @param campo Campo onde ocorreu o erro, para caso falhe a validação.
+ *
+ * @returns 1 para válido, 0 para invalidado.
+ */
 int validate_time(const struct tm* time, char *campo);
 
-// Verifica se o a variável string está vazia
+/**
+ * Checa se a string é vazia.
+ *
+ * @param name String a ser testada.
+ * @param id Número de identificação da estrutura.
+ *
+ * @returns 1 para válido, 0 para invalidado.
+ */
 int validate_name(const char* name, int id);
 
+/**
+ * Checa se a string é vazia.
+ *
+ * @param str String a ser testada.
+ * @param message Mensagem caso a validação falhe.
+ *
+ * @returns 1 para válido, 0 para invalidado.
+ */
+int validate_string(const char* str, char* message);
+
+/**
+ * Gera aleatóriamente um número.
+ *
+ * @param max_number Número máximo.
+ *
+ * @returns Número inteiro.
+ */
 int get_random_number(int max_number);
 
+/**
+ * Gera aleatóriamente um número float.
+ *
+ * @param max_number Número máximo.
+ *
+ * @returns Número de ponto flutuante.
+ */
 float get_random_float_number(float max_number);
 
-struct tm create_date(int ano, int mes, int dia);
+/**
+ * Verifica se o número de dias está correto para aquele mês.
+ *
+ * @param month Mêses no ano, desde janeiro (0 - 11).
+ * @param day Dias do mês (0 - 31).
+ *
+ * @returns Retorna o dia inserido se válido, caso não seja, retorna o número máximo de dias naquele mês.
+ */
+int validate_day_of_month(int month, int day);
 
+/**
+ * Cria uma data com os paramentros inseridos.
+ *
+ * @param year Ano, anos passados após 1900 (0 - 124).
+ * @param month Mêses no ano, desde janeiro (0 - 11).
+ * @param day Dias do mês (0 - 31).
+ *
+ * @returns Data/Hora.
+ */
+struct tm create_date(int year, int month, int day);
+
+/**
+ * Retorna data/hora atual.
+ *
+ * @returns Data/Hora.
+ */
 struct tm* get_time();
 
+/**
+ * Retorna um timestamp em string no formato YY-mm-dd HH:MM:SS
+ *
+ * @param date Variável datetime.
+ *
+ * @returns em string.
+ */
 char* get_timestamp_from_datetime(struct tm* date);
 
+/**
+ * Retorna data em string no formato YY-mm-dd de uma variável datetime
+ *
+ * @param date Variável datetime.
+ *
+ * @returns Data em string.
+ */
 char* get_date_from_datetime(struct tm* date);
 
+/**
+ * Imprime na tela as propriedades de acordo com o tipo da estrutura.
+ *  
+ * @param type Tipo da estrutura.
+ * @param p Tipo da estrutura.
+ */
 void print_by_struct_type(StructType type, void *p);
 
+/**
+ * Retorna uma string com as propriedades de acordo com o tipo da estrutura.
+ *  
+ * @param type Tipo da estrutura.
+ * @param p Tipo da estrutura.
+ * 
+ * @return String com as propriedades da estrutura do ponteiro.
+ */
 char* output_by_struct_type(StructType type, void *p);
 
-void db_save(char *content, StructType type);
+/**
+ * Retorna em string o endereço do arquivo db da estrutura.
+ * *  
+ * @param type Tipo da estrutura.
+ * 
+ * @returns String com endereço do arquivo db da estrutura.
+ */
+char* get_db_path(StructType type);
 
+/**
+ * Salva o conteúdo em um arquivo de acordo com o tipo da estrutura.
+ *  
+ * @param content String com o texto a ser armazenado no arquivo.
+ * @param type Tipo da estrutura.
+ */
+void db_save(StructType type, char *content);
+
+/**
+ * Libera memória da estrutura devido a seu tipo, usando as funções corretas 
+ * 
+ * @param type Tipo do ponteiro.
+ * @param p Ponteiro a ter sua memória liberada.
+ */
 void free_by_struct_type(StructType type, void *p);
+
+/**
+ * Calcula quanto tempo passou desde um ponto anterior onde originou a variavel t
+ * 
+ * @param t  Ponto anterior.
+ */
+double time_passed(clock_t t);
+
+/**
+ * Executa testando a função q_enqueue_prio(), para checagem da prioridade do nível de gravidade de cada Exame.
+ *  
+ */
+void test_enqueue_prio();
 
 #endif
--- src/functions.c
+++ src/functions.c
@@ -10,35 +10,36 @@
 #include <stdlib.h> // malloc, EXIT_FAILURE
 #include <string.h> // strcmp
 #include <stdio.h> // printf
-#include <windows.h>
 
-// Mostra uma mensagem de erro e para o programa com o código de erro
+// Para a execução do programa com o código de erro.
 void error_exit(int err_code){
-    exit(err_code);
+    exit(err_code); 
 }
 
+// Imprime uma mensagem na tela com id.
 void error_message_id(const char *msg, int id){
     printf("%s: %d.", msg, id);
 }
 
+// Imprime uma mensagem na tela com especificação do campo.
 void error_message_field(const char *msg, char *campo){
     printf("%s: %s.", msg, campo);
 }
 
-// Verifica se o inteiro id é válido, maior que zero
+// Verifica se o número inteiro id é válido, maior que zero.
 int validate_id(int num, char *campo){
     if ( num > 0 ) {
         return 1;
     } else {
-        // Mensagem de erro
+        // Mensagem de erro.
         error_message_field("Erro.\nNumero inválido em campo", campo);
-        // Função para mostrar o erro e finaliza execução
+        // Finaliza execução.
         error_exit(EXIT_FAILURE);
         return 0;
     }
 }
 
-// Verifica se o Dia, Mês e Ano é igual a zero  
+// Verifica se o Dia, Mês e Ano é igual a zero.
 int validate_time(const struct tm* time, char *campo){
 
     if( time->tm_year == 0 &&
@@ -48,152 +49,182 @@ int validate_time(const struct tm* time, char *campo){
         char msg[] = "Data/Hora não informada no campo: ";
         strcat(msg, campo);
 
-        // Mensagem de erro
+        // Mensagem de erro.
         error_message_field("Erro.\nData/Hora não informada no campo", campo);
-        // Função para mostrar o erro e finaliza execução
+        // Função para mostrar o erro e finaliza execução.
         error_exit(EXIT_FAILURE);
         return 0;
     } 
     return 1;
 }
 
-// Verifica se o a variável string está vazia
+// Verifica se o a variável name está vazia e especifica o id.
 int validate_name(const char* name, int id){
     if( strcmp(name, "") == 0 || strcmp(name, " ") == 0){
-        // Mensagem de erro
+        // Mensagem de erro.
         error_message_id("Erro.\nNão foi possível encontrar o nome do paciente com id", id);
-        // Função para mostrar o erro e finaliza execução
+        // Função para mostrar o erro e finaliza execução.
         error_exit(EXIT_FAILURE);
         return 0;
     }
     return 1;
 }
 
-int get_random_number(int max_number){
-    srand(time(NULL));
-    return rand() % (max_number + 1);
+// Verifica se o a variável string está vazia e mostra mensagem de erro.
+int validate_string(const char* str, char* message){
+    if( strcmp(str, "") == 0 || strcmp(str, " ") == 0){
+        // Mensagem de erro
+        printf("Erro.\nString vazia. %s", message);
+        // Função para mostrar o erro e finaliza execução.
+        error_exit(EXIT_FAILURE);
+        return 0;
+    }
+    return 1;
 }
 
+// Gera um número aleatório com número máximo especificado.
+int get_random_number(int max_number){
+    srand(time(NULL));
+    return rand() % (max_number + 1); // Retorna o número aleatório.
+}
+
+// Gera um número de ponto flutuante aleatório com número máximo especificado.
 float get_random_float_number(float max_number){
     srand((unsigned int)time(NULL));
 
     return ((rand()/(float)(RAND_MAX)) * max_number);
 }
 
+// Verifica se o número de dias está correto para aquele mês.
 int validate_day_of_month(int month, int day){
-    if(month > 11)
+    if(month > 11) // Verifica se o número do mês é válido.
         month = 11;
 
+    // Máximo de dias em cada mês
     int DAYS_IN_MONTH[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
 
+    // Checa se número de dias equivale ao mês.
     if(day > DAYS_IN_MONTH[month])
-        return DAYS_IN_MONTH[month];
+        return DAYS_IN_MONTH[month]; // Retorna o máximo de dias naquele mês.
     else
-        return day;
+        return day; // Retorna o dia inserido.
 }
 
+// Cria e retorna uma data com os parâmetros especificados.
 struct tm create_date(int year, int month, int day){
-    struct tm date = {0}; // Inicializar a estrutura com zeros
+    struct tm date = {0}; // Inicializar a estrutura com zeros.
 
     day = validate_day_of_month(month, day);
 
-    date.tm_year = year; // Ano 1990
-    date.tm_mon = month; // Junho (0-indexed)
+    date.tm_year = year; // Anos após 1990
+    date.tm_mon = month; // (0-indexed)
     date.tm_mday = day; // Dia 
    
-    return date;
+    return date; // Retorna a data.
 }
 
+// Retorna a Data/Hora atual.
 struct tm* get_time(){
-    // Definindo uma data e hora fictícias para o exame
     time_t current_time;
-    current_time = time(NULL); // Obter o tempo atual em segundos desde 01/01/1970
-    return localtime(&current_time); // Converter o tempo para a hora local
+    current_time = time(NULL); // Obter o tempo atual em segundos desde 01/01/1970.
+    return localtime(&current_time); // Retorna o tempo convertido para a hora local.
 }
 
-// YY-mm-dd HH:MM:SS
+// Retorna um timestamp em string no formato YY-mm-dd HH:MM:SS.
 char* get_timestamp_from_datetime(struct tm* date){
+    // Alocação de memória para o ponteiro.
     char *output = (char *)malloc(sizeof(char) * 32);
-    // date->tm_year retorna quantos anos desde 1900
+    // Estrutura da string.
     sprintf(output, "%d-%d-%d %d:%d:%d", (1900 + date->tm_year), date->tm_mon, date->tm_mday, date->tm_hour, date->tm_min, date->tm_sec);
-    return output;
+    return output; // Retorna o ponteiro.
 }
 
-// YY-mm-dd
+// Retorna data em string no formato YY-mm-dd de uma variável datetime.
 char* get_date_from_datetime(struct tm* date){
+    // Alocação de memória do ponteiro.
     char *output = (char *)malloc(sizeof(char) * 32);
-    // date->tm_year retorna quantos anos desde 1900
+    // date->tm_year retorna quantos anos desde 1900.
     sprintf(output, "%d-%d-%d", (1900 + date->tm_year), date->tm_mon, date->tm_mday);
-    return output;
+    return output; // Retorna o ponteiro.
 }
 
+// Imprime na tela as informações das estruturas
+// É usado a função correta de cada estruturda
 void print_by_struct_type(StructType type, void *p) {
     switch (type) {
         case TYPE_PATIENT:
-            print_patient(p);
+            print_patient(p); // Patient
             break;
 
         case TYPE_EXAM:
-            print_exam(p);
+            print_exam(p); // Exam
             break;
 
         case TYPE_REPORT:
-            print_report(p);
+            print_report(p); // Report
             break;
 
         default:
+            printf("Não foi possível imprimir essa estrutura.");
+            printf("\nStructType não encontrado.");
             break;
     }
 }
 
+// Retorna as informações das estruturas em string
+// É usado a função correta de cada estruturda
 char* output_by_struct_type(StructType type, void *p) {
     switch (type) {
         case TYPE_PATIENT:
-                return patient_output(p);
+                return patient_output(p); // Patient
                 break;
 
         case TYPE_EXAM:
-            return exam_output(p);
+            return exam_output(p); // Exam
             break;
 
         case TYPE_REPORT:
-            return report_output(p);
+            return report_output(p); // Report
             break;
     }
+    return "Error. StructType inválida.";
 }
 
-void db_save(char *content, StructType type){
-    char *path;
+// Salva os registros de novas estruturas em seus arquivos db
+char* get_db_path(StructType type){
+
+    // Resolve o endereço do arquivo pelo tipo da estrutura
     switch (type) {
         case TYPE_PATIENT:
-            printf("flag patient");
-            path = "./src/static/db_patient.txt";
-            break;
+            return "./src/static/db_patient.txt";
         
         case TYPE_EXAM:
-            printf("flag exam");
-            path = "./src/static/db_exam.txt";
-            break;
+            return "./src/static/db_exam.txt";
         
         case TYPE_REPORT:
-            printf("flag report");
-            path = "./src/static/db_report.txt";
-            break;
-
-        default:
-            break;
+            return "./src/static/db_report.txt";
     }
+    return "";
 
+}
+
+// Adiciona uma linha no arquivo especificado.
+void db_save(StructType type, char* content){
+    // Endereço do arquivo.
+    char *path = get_db_path(type); 
     FILE *file_pointer; 
 
-    file_pointer = fopen(path, "a");
-
+    // Abre o arquivo em appending mode.
+    file_pointer = fopen(path, "a"); 
+    // Adiciona a linha ao arquivo.
     fprintf(file_pointer, "%s\n", content);
-
+    // Fecha o arquivo.
     fclose(file_pointer);
 }
 
+// Libera memória da estrutura devido a seu tipo, usando as funções corretas 
 void free_by_struct_type(StructType type, void *p) {
+    // Decide qual função usar baseado no tipo da estrutura.
     switch (type) {
         case TYPE_PATIENT:
             destroy_patient(p);
@@ -206,8 +237,68 @@ void free_by_struct_type(StructType type, void *p) {
         case TYPE_REPORT:
             destroy_report(p);
             break;
-
-        default:
-            break;
     }
+}
+
+
+// Calcula quanto tempo passou desde um ponto anterior onde originou a variavel t
+double time_passed(clock_t t){
+    clock_t t2 = clock() - t; // Calcula o tempo atual e o tempo da variavel t
+    double time_taken = ((double)t2)/CLOCKS_PER_SEC; // Converte em segundos dividindo por CLOCKS_PER_SEC
+
+    return time_taken; // Retorna a diferença de tempo
+}
+
+// Cria exames de prioridades diferentes, de forma não sequencial para checar se a lista de prioridade de exame esta funcionando.
+void test_enqueue_prio(){
+
+    // id counters
+    int patient_id_counter = 1;
+    int exam_id_counter = 1;
+
+    struct tm birthdate = create_date(get_random_number(124), get_random_number(11), get_random_number(31));
+
+    Patient *patient = create_patient(patient_id_counter++, "João Silva", &birthdate);
+
+    Queue *ExamPriorityQueue = q_create();
+
+    char* condition = "Muito sono";
+
+    Exam *new_aexam;
+    new_aexam = create_exam(exam_id_counter++, get_patient_id(patient), (1), get_time());
+    set_exam_condition(new_aexam, 10, condition, 4);
+
+    q_enqueue_exam_prio(ExamPriorityQueue, TYPE_EXAM, new_aexam);
+
+    new_aexam = create_exam(exam_id_counter++, get_patient_id(patient), (1), get_time());
+    set_exam_condition(new_aexam, 10, condition, 1);
+
+    q_enqueue_exam_prio(ExamPriorityQueue, TYPE_EXAM, new_aexam);
+
+    new_aexam = create_exam(exam_id_counter++, get_patient_id(patient), (1), get_time());
+    set_exam_condition(new_aexam, 10, condition, 5);
+
+    q_enqueue_exam_prio(ExamPriorityQueue, TYPE_EXAM, new_aexam);
+
+    new_aexam = create_exam(exam_id_counter++, get_patient_id(patient), (1), get_time());
+    set_exam_condition(new_aexam, 10, condition, 3);
+
+    q_enqueue_exam_prio(ExamPriorityQueue, TYPE_EXAM, new_aexam);
+
+    new_aexam = create_exam(exam_id_counter++, get_patient_id(patient), (1), get_time());
+    set_exam_condition(new_aexam, 10, condition, 2);
+
+    q_enqueue_exam_prio(ExamPriorityQueue, TYPE_EXAM, new_aexam);
+
+    new_aexam = create_exam(exam_id_counter++, get_patient_id(patient), (1), get_time());
+    set_exam_condition(new_aexam, 10, condition, 5);
+
+    q_enqueue_exam_prio(ExamPriorityQueue, TYPE_EXAM, new_aexam);
+
+    new_aexam = create_exam(exam_id_counter++, get_patient_id(patient), (1), get_time());
+    set_exam_condition(new_aexam, 10, condition, 5);
+
+    q_enqueue_exam_prio(ExamPriorityQueue, TYPE_EXAM, new_aexam);
+
+    q_print(ExamPriorityQueue);
 }
